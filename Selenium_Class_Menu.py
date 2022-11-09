21484a9a452770f0f538d7fac7409d533275f87c
Cesar Eduardo Muñoz Chavez <90935672+DreamnovaCesar@users.noreply.github.com>
2022-11-08 18:48:50 -0600

    Add classes

---
+++ Selenium_Class_Menu.py
@@ -0,0 +1,48 @@
+from Selenium_Class_MakeGirlMOE import 
+
+class Menu():
+
+    def __init__(self, **kwargs) -> None:
+        pass
+
+    def __repr__(self):
+
+        kwargs_info = '';
+
+        return kwargs_info
+
+    def __str__(self):
+        pass
+    
+    @staticmethod
+    def create_objects_2D(Folder_2D_):
+        pass
+
+    @Utilities.time_func  
+    def menu(self):
+
+        while(True):
+            
+            Asterisk = 60;
+
+            print("\n");
+
+            print("*" * Asterisk);
+            print('What do you want to do:');
+            print("*" * Asterisk);
+            print('\n');
+            print('1: ');
+
+            print('\n');
+            print("*" * Asterisk);
+
+            Options = input('Option: ');
+
+            if(Options == '1'):
+                pass
+
+            elif(Options == 'c'):
+
+                break;
+
+        return -1
